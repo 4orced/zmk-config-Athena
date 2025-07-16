4435f20be9b6324f815250fc12a0c02411d7bb64
4orced <Reload@live.com>
2025-07-15 20:01:41 -0600

    Update display.c

--- src/display.c
+++ src/display.c
@@ -1,15 +1,19 @@
+#include <lvgl.h>
+#include <zmk/display.h>
+#include <zmk/display/widgets/status.h>
 #include "logo.h"
 
-const LV_ATTRIBUTE_MEM_ALIGN LV_ATTRIBUTE_LARGE_CONST uint8_t my_logo_map[] = {
-    0x00, 0xFF, 0x00, 0xFF,  // Very tiny fake bitmap data
-    0xFF, 0x00, 0xFF, 0x00
-};
+static sys_slist_t widgets = SYS_SLIST_STATIC_INIT(&widgets);
 
-const lv_img_dsc_t my_logo = {
-    .header.always_zero = 0,
-    .header.w = 4,
-    .header.h = 2,
-    .data_size = sizeof(my_logo_map),
-    .header.cf = LV_IMG_CF_INDEXED_1BIT,
-    .data = my_logo_map,
-};
+int zmk_display_init() {
+    struct zmk_widget_status *status_widget = zmk_widget_status_create();
+    zmk_widget_status_init(status_widget, lv_scr_act());
+    sys_slist_append(&widgets, &status_widget->node);
+
+    // Add logo
+    lv_obj_t *logo = lv_img_create(lv_scr_act());
+    lv_img_set_src(logo, &my_logo);
+    lv_obj_align(logo, LV_ALIGN_TOP_LEFT, 0, 0);
+
+    return 0;
+}
